4fbe79fae10a0e018d8f720ccdb95e026d8ba3c8
Hunter Williams <hunter.williams@wegmans.com>
2024-04-15 18:00:43 -0400

    exercise 5

---
+++ src/components/Banner/Banner.js
@@ -0,0 +1,29 @@
+import React from 'react';
+
+export default function Banner( { won, answer, numGuesses } ) {
+
+
+    const happyBanner = (
+        <div className="happy banner">
+            <p>
+                <strong>Congratulations!</strong> Got it in {' '} <strong>{numGuesses} guesses</strong>.
+            </p>
+        </div>
+    )
+
+    const sadBanner = (
+        <div className="sad banner">
+            <p>Sorry, the correct answer is <strong>{answer}</strong>.</p>
+        </div>
+    )
+
+    return (
+        <>
+        {won 
+        ? happyBanner
+        : sadBanner}
+        </>
+    )
+    
+}
+
--- src/components/Game/Game.js
+++ src/components/Game/Game.js
@@ -1,9 +1,10 @@
 import React from 'react';
 import GuessInput from '../GuessInput';
 import GuessTrack from '../GuessTrack';
+import Banner from '../Banner';
 import { sample } from '../../utils';
 import { WORDS } from '../../data';
-
+import { NUM_OF_GUESSES_ALLOWED } from '../../constants.js';
 // Pick a random word on every pageload.
 const answer = sample(WORDS);
 // To make debugging easier, we'll log the solution in the console.
@@ -13,22 +14,33 @@ console.info({ answer });
 
 function Game() {
   
+  
+  
   const [ guesses, setGuesses ] = React.useState([]);
+  
+  const [ gameOver, setGameOver ] = React.useState(false);
   
   function handleSubmit(guess){
   
-    const nextGuesses = [
-       ...guesses,
-       guess
-    ];
-    
-    setGuesses(nextGuesses);
+    if(guesses.length === NUM_OF_GUESSES_ALLOWED) {
+      setGameOver(true);
+    } 
+    else {
+      const nextGuesses = [
+        ...guesses,
+        guess
+     ];
+     setGuesses(nextGuesses);
+     if(answer === guess) setGameOver(true);
+    }
   
   }
   return (
     <>
       <GuessTrack answer={answer} guesses={guesses} />
       <GuessInput handleSubmit={handleSubmit} />
+      {gameOver && <Banner won={guesses.length < NUM_OF_GUESSES_ALLOWED} answer={answer} numGuesses={guesses.length} />}
+      
     </>
   );
 } 
