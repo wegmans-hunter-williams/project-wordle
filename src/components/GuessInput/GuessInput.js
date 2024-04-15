fd89ff85fc3c8a464181c672314a321a0e21d51e
Hunter Williams <hunter.williams@wegmans.com>
2024-04-15 14:05:27 -0400

    exercise 1b

--- src/components/GuessInput/GuessInput.js
+++ src/components/GuessInput/GuessInput.js
@@ -11,7 +11,7 @@ export default function GuessInput() {
         console.log({
             guess
         });
-        setGuess('');
+        
     }
     /* 
         @todo refactor minLength validation
